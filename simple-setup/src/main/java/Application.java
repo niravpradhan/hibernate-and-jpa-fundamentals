b1c9bd6a5dae4b51066c12c666f9dffb927df647
nirav <nirav_pradhan@yahoo.co.in>
2021-02-11 09:35:08 +0530

    chapter3-persistence-methods

--- simple-setup/src/main/java/Application.java
+++ simple-setup/src/main/java/Application.java
@@ -1,11 +1,28 @@
 import me.niravpradhan.data.HibernateUtil;
+import me.niravpradhan.data.entities.User;
 import org.hibernate.Session;
+
+import java.time.LocalDateTime;
 
 public class Application {
 
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
+        session.getTransaction().begin();
+
+        User user = new User();
+        user.setCreatedBy("Nirav");
+        user.setCreatedDate(LocalDateTime.now());
+        user.setLastUpdatedBy("Nirav");
+        user.setLastUpdatedDate(LocalDateTime.now());
+        user.setBirthDate(LocalDateTime.now());
+        user.setEmailAddress("nirav.pradhan@gmail.com");
+        user.setFirstName("Nirav");
+        user.setLastName("Pradhan");
+
+        session.save(user);
+
+        session.getTransaction().commit();
         session.close();
     }
 }
